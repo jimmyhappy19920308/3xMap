69a43044912e949b1e1357a857083f5850aa567f
yingming25 <karcnonf@gmail.com>
2020-09-20 11:57:27 +0800

    載入地圖與圖資

--- all.js
+++ all.js
@@ -1,9 +1,18 @@
 const sidebarSwitch = document.querySelector('.sidebarSwitch');
 const sidebar = document.querySelector('.sidebar');
-const map = document.getElementById('map');
+const mapElement = document.getElementById('map');
 
 sidebarSwitch.addEventListener('click', function(e){
   sidebar.classList.toggle('active');
   this.classList.toggle('active');
-  map.classList.toggle('active');
+  mapElement.classList.toggle('active');
 });
+
+const map = L.map('map', {
+  center: [22.604799,120.2976256],
+  zoom: 16
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
